819400c5bd934cfefd90d7add6aaf9ead044b87f
Kaio Silveira <silveira.kaio@icloud.com>
2024-09-03 17:53:10 +0100

    ex-1: introduce subclasses of `Bird`

--- src/functions-into-class-hierarchy/bird/index.js
+++ src/functions-into-class-hierarchy/bird/index.js
@@ -29,3 +29,9 @@ export class Bird {
     }
   }
 }
+
+export class EuropeanSwallow extends Bird {}
+
+export class AfricanSwallow extends Bird {}
+
+export class NorwegianBlueParrot extends Bird {}
